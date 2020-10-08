d72a58d92ea548c5cc5931d631d71de613b40056
Aleksandr Izmaylov <zinoviy23@gmail.com>
2020-10-08 17:30:10 +0300

    script for archive

---
+++ build_acrchive.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+folder=Seminar2
+
+mkdir $folder
+cp -r gradle $folder/gradle
+cp -r src $folder/src
+cp build.gradle $folder/build.gradle
+cp gradle.properties $folder/gradle.properties
+cp gradlew $folder/gradlew
+cp gradlew.bat $folder/gradlew.bat
+cp settings.gradle $folder/settings.gradle
+cp -r build/distributions $folder/executable
+
+zip ${folder}_Aleksand_Izmaylov $folder -r
